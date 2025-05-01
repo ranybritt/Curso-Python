0c276d0f9a40c9ea18bed9156a642a0fb4d33fd5
Raniely <ranielyferreiraa@gmail.com>
2025-05-01 20:45:19 -0300

    Update 038 Comparando números.py

--- Mundo 2/038 Comparando números.py
+++ Mundo 2/038 Comparando números.py
@@ -1,6 +1,5 @@
 ''' Exercício Python 038:
-Escreva um programa que leia dois números inteiros e compare-os.
-Mostrando na tela uma mensagem:
+Escreva um programa que leia dois números inteiros e compare-os. Mostrando na tela uma mensagem:
 – O primeiro valor é maior
 – O segundo valor é maior
 – Não existe valor maior, os dois são iguais
