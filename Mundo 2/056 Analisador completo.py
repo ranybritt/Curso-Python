9e1742d1e81aec663db85c89ed4b43ec88629e0f
ranybritt <ranielyferreiraa@gmail.com>
2025-05-06 15:29:04 -0300

    Atualizações Mundo 2

--- Mundo 2/056 Analisador completo.py
+++ Mundo 2/056 Analisador completo.py
@@ -6,6 +6,7 @@ a média de idade do grupo, qual é o nome do homem mais velho e quantas mulhere
 somaidd = 0
 maior = 0
 velho = ''
+menos20 = 0
 for p in range(1, 5):
     print(f'------- {p}° pessoa -------')
     nome = str(input('Nome: '))
@@ -15,6 +16,9 @@ for p in range(1, 5):
     if sexo in 'Mm' and idade > maior:
         velho = nome
         maior = idade
+    if sexo in 'Ff' and idade < 20:
+        menos20 += 1
 mediaidd = somaidd / 4
 print(f'\nA média da idade do grupo é {mediaidd:.1f} anos')
 print(f'O homem mais velho do grupo é {velho} com {maior} anos.')
+print(f'Total de mulheres com menos de 20 anos: {menos20}')
