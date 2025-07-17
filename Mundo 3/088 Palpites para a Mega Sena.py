cf85b53dc86db753327842c824c42caf357929c2
ranybritt <ranielyferreiraa@gmail.com>
2025-07-16 21:40:28 -0300

    Aula 18

--- Mundo 3/088 Palpites para a Mega Sena.py
+++ Mundo 3/088 Palpites para a Mega Sena.py
@@ -5,4 +5,23 @@ e vai sortear 6 números entre 1 e 60 para cada jogo,
 cadastrando tudo em uma lista composta.
 	Resolução:
 '''
+from random import randint
 
+jogos = list()
+print("-"*30)
+print("     Sorteio da Mega Sena     ")
+print("-"*30)
+quant = int(input("Quantos jogos deseja sortear: "))
+for c in range(0, quant):
+    lista = []
+    while len(lista) < 6:
+        num = randint(1, 60)
+        if num not in lista:
+            lista.append(num)
+    lista.sort()
+    jogos.append(lista)
+print("-"*30)
+print(f"SORTEANDO {quant} JOGOS")
+print("-"*30)
+for i, l in enumerate(jogos, 1):
+    print(f"Jogo {i}: {l}")
--- Mundo 3/089 Boletim com listas compostas.py
+++ Mundo 3/089 Boletim com listas compostas.py
@@ -5,6 +5,31 @@ um boletim contendo a média de cada um e permita que
 o usuário possa mostrar as notas de cada aluno individualmente.
 	Resolução:
 '''
-
-
+boletim = list()
+media = 0
+while True:
+	nome = str(input("Nome: "))
+	nota1 = float(input("Primeira nota: "))
+	nota2 = float(input("Segunda nota: "))
+	media = (nota1 + nota2) / 2
+	boletim.append([nome, [nota1, nota2], media])
+	resp = " "
+	while resp not in "SN":
+		resp = str(input("Deseja adicionar mais um aluno: [S/N]")).strip().upper()[0]
+	if resp == "N":
+		break
+print("-"*40)
+print(f"{'Nº.':<4}{'ALUNO':<15}{'MÉDIA':>8}")
+print("-"*40)
+for i, a in enumerate(boletim):
+    print(f"{i:<4}{a[0]:<15}{a[2]:>8.1f}")
+while True:
+    print("-"*40)
+    opc = int(input("Mostrar notas de qual aluno? (999 interrompe): "))
+    if opc == 999:
+        print("FINALIZANDO...")
+        break
+    if opc >= 0 and opc < len(boletim):
+        print(f"Notas de {boletim[opc][0]} são: {boletim[opc][1]}")
+print("<<< VOLTE SEMPRE >>>")
 
